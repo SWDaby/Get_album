fa81470e5d7bafdba68c430bb9bbcf6f933d7212
daby <zf.daby@gamil.com>
2022-06-08 23:23:46 +0800

    优化请求头

--- Get_album.py
+++ Get_album.py
@@ -26,7 +26,6 @@ def getCookie():
     return  cookies_str
 
 
-
 #定义生成文件方法
 def get_pic_by_url(dir_name,url):
     #print(os.getlogin())
@@ -72,69 +71,70 @@ def get_dir_name(number,headers):
     return name
 
 
-# 指定ajax-get请求的url（通过抓包进行获取）
-url = 'https://api.vc.bilibili.com/link_draw/v1/doc/doc_list?'
-headers = {'User-Agent': UserAgent().random,
-           "cookie":getCookie()
-           #'cookie':'buvid3=22B20D8F-B1D1-7026-28C3-CF45B43F6B1E18520infoc; i-wanna-go-back=-1; b_ut=7; CURRENT_BLACKGAP=0; CURRENT_FNVAL=80; innersign=0'
-                }
+if __name__ == '__main__':
 
-id_number=input("请输入up主页号:")
-page=input("请输入要抓取的页数:")
-dirname= get_dir_name(id_number, headers)
-print("将生成文件在："+str(dirname))
-# 封装ajax的get请求携带的参数(从抓包工具中获取) 封装到字典
+    # 指定ajax-get请求的url（通过抓包进行获取）
+    url = 'https://api.vc.bilibili.com/link_draw/v1/doc/doc_list?'
+    headers = {'User-Agent': UserAgent().random,
+               "cookie":getCookie()
+               #'cookie':'buvid3=22B20D8F-B1D1-7026-28C3-CF45B43F6B1E18520infoc; i-wanna-go-back=-1; b_ut=7; CURRENT_BLACKGAP=0; CURRENT_FNVAL=80; innersign=0'
+                    }
 
-for i in range(int(page)):
-                param = {
-                 'uid':id_number,
-                 'page_num':i,
-                 'page_size':'30',
-                 'biz':'all'
-  
-                }
-                
-# 定制请求头信息，相关的头信息必须封装在字典结构中
-               
-                
-# 发起ajax的get请求还是用get方法
-                response = requests.get(url=url,params=param,headers=headers)
+    id_number=input("请输入up主页号:")
+    page=input("请输入要抓取的页数:")
+    dirname= get_dir_name(id_number, headers)
+    print("将生成文件在："+str(dirname))
+    # 封装ajax的get请求携带的参数(从抓包工具中获取) 封装到字典
 
- 
-# 获取响应内容：响应内容为json字符串
-                data = response.text
-                data2 = json.loads(data)
+    for i in range(int(page)):
+                    param = {
+                     'uid':id_number,
+                     'page_num':i,
+                     'page_size':'30',
+                     'biz':'all'
 
-                print(data2)
-#print("============================================================")
-#for data2_dict in data2:
-                
-                data3=jsonpath(data2,'$..items[*].doc_id')
-  
-                print (data3)
-                print('---------------------------------------------')
+                    }
 
-                url2='https://api.vc.bilibili.com/link_draw/v1/doc/detail?'
-                for index in range(len(data3)):
-                               print ('当前动态id :', data3[index])
-                               param2={
-                                 "doc_id":data3[index]
-                                               }
-                               
-                               #headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36 Edg/83.0.478.44',}
+    # 定制请求头信息，相关的头信息必须封装在字典结构中
 
-                               response2 = requests.get(url=url2,params=param2,headers=headers)
-                               data4=response2.text
-                               data5=json.loads(data4)
-                               data5=jsonpath(data5,'$..pictures[*].img_src')
-                               for index2 in range(len(data5)):
-                                               print('图片链接:',data5[index2])
-                                               get_pic_by_url(dirname,data5[index2])
-                               print('```````````````````````````````````````````````````````')
-                               
-                   
- 
-print ("Good bye!")
+
+    # 发起ajax的get请求还是用get方法
+                    response = requests.get(url=url,params=param,headers=headers)
+
+
+    # 获取响应内容：响应内容为json字符串
+                    data = response.text
+                    data2 = json.loads(data)
+
+                    #print(data2)
+    #print("============================================================")
+    #for data2_dict in data2:
+
+                    data3=jsonpath(data2,'$..items[*].doc_id')
+
+                    print(data3)
+                    print('---------------------------------------------')
+
+                    url2='https://api.vc.bilibili.com/link_draw/v1/doc/detail?'
+                    for index in range(len(data3)):
+                                   print ('当前动态id :', data3[index])
+                                   param2={
+                                     "doc_id":data3[index]
+                                                   }
+
+
+                                   response2 = requests.get(url=url2,params=param2,headers=headers)
+                                   data4=response2.text
+                                   data5=json.loads(data4)
+                                   data5=jsonpath(data5,'$..pictures[*].img_src')
+                                   for index2 in range(len(data5)):
+                                                   print('图片链接:',data5[index2])
+                                                   get_pic_by_url(dirname,data5[index2])
+                                   print('```````````````````````````````````````````````````````')
+
+
+
+    print ("Good bye!")
 
 
 
